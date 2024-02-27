ae8d6299d0df065d5016225a9f0bad649a195d80
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-02-27 11:52:22 +0530

    init

---
+++ Stack/Implementation/implementation_Using_Queue.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Stack{
+    
+    queue<int>q;
+
+    public:
+
+        void Push(int x){
+            int s = q.size();
+            q.push(x);
+            for(int i=0; i<s; i++){  // reinserting all the elements after pushing so that it mimics a stack
+                q.push(q.front());
+                q.pop();
+            }
+        }
+
+        int Pop(){
+            int n = q.front();
+            q.pop();
+            return n;
+        }
+
+        int Top(){
+            return q.front();
+        }
+
+        int Size(){
+            return q.size();
+        }
+
+        bool Empty(){
+            return q.empty() ? true : false;
+        }
+
+};
+
+
+
+int main(){
+
+    Stack s;
+
+    s.Push(3);
+    s.Push(2);
+    s.Push(4);
+    s.Push(1);
+    cout << "Top of the stack: " << s.Top() << endl;
+    cout << "Size of the stack before removing element: " << s.Size() << endl;
+    cout << "The deleted element is: " << s.Pop() << endl;
+    cout << "Top of the stack after removing element: " << s.Top() << endl;
+    cout << "Size of the stack after removing element: " << s.Size() << endl;
+    cout << "The deleted element is: " << s.Pop() << endl;
+    cout << "The deleted element is: " << s.Pop() << endl;
+    cout << "Top of the stack: " << s.Top() << endl;
+    cout << "is the Stack empty: " << s.Empty() << endl;
+    cout << "The deleted element is: " << s.Pop() << endl;
+    cout << "Top of the stack: " << s.Top() << endl;
+    cout << "is the Stack empty: " << s.Empty() << endl;
+    return 0;
+}
